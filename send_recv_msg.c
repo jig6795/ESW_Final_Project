380da57a098dbbafef62cfe078b11af3f0a86808
Ilkgu <wjd1rb@gmail.com>
2018-11-28 09:43:13 +0000

    Add send_message_function

---
+++ send_recv_msg.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define BUF_SIZE 100
+#define NAME_SIZE 20
+
+char msg[BUF_SIZE];
+char name[NAME_SIZE] = "[DEFAULT]";
+
+void* send_msg(void* arg)
+{
+  int sock = *((int*)arg);
+
+  char name_msg[NAME_SIZE+BUF_SIZE];
+
+  while(1)
+  {
+    fgets(msg,BUF_SIZE,stdin);
+
+    if(!strcmp(msg,"q\n")||strcmp(msg,"Q\n"))
+    {
+      close(sock);
+      exit(1);
+    }
+    sprintf(name_msg,"%s > %s",name,msg);
+    write(sock,name_msg,strlen(name_msg));
+    memset(msg,0,sizeof(msg));
+  }
+
+  return NULL;
+}
+
+int main(void)
+{
+  while(1)
+  {
+    printf("1");
+    return 0;
+  }
+}
