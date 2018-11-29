5eb45b52691c090a5481e775710499d431bdb4fd
Ilkgu <wjd1rb@gmail.com>
2018-11-29 04:31:56 +0000

    Add receive message function

--- client_side/send_recv_msg.c
+++ client_side/send_recv_msg.c
@@ -32,6 +32,26 @@ void* send_msg(void* arg)
   return NULL;
 }
 
+void* recv_msg(void* arg)
+{
+  int sock = *((int*)arg);
+  char name_msg[NAME_SIZE+BUF_SIZE];
+  int str_len;
+
+  while(1)
+  {
+    str_len = read(sock,name_msg,NAME_SIZE+BUF_SIZE-1);
+    if(str_len == -1)
+    {
+      return (void*)-1;
+    }
+    name_msg[str_len]=0;
+    fputs(name_msg,stdout);
+  }
+  return NULL;
+}
+
+
 int main(void)
 {
   while(1)
